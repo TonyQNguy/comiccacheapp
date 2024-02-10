5debb5349473dcf5bf420cfe2e20a23668cfec76
MichaelMandle <m1mandle1@gmail.com>
2024-02-10 05:18:32 -0500

    Working on the comic details. Slowly adding in the styling.

--- lib/comiccache_details.dart
+++ lib/comiccache_details.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:comiccacheapp/info.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:comicbookstore/info.dart';
 
-// work on the details page later, work on the front page first
-class ComicBookDetail extends StatelessWidget {
-  //const ComicBookDetail({super.key});
-
+class BookDetail extends StatelessWidget {
   final Book book;
 
-  ComicBookDetail({required this.book});
+  BookDetail({required this.book});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            book.image,
+            fit: BoxFit.fitWidth,
+          ),
+          Positioned(
+            top: 48,
+            left: 16,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white,
+                ),
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.all(16),
+              color: Colors.grey[200],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    book.title,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    book.author.fullname,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    book.description,
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
