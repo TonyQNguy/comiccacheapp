c27d64d079716b53e48ed34d8f5452211eb3d597
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-02-09 19:46:01 -0500

    added a new file for the details page of the comic

---
+++ lib/comiccache_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:comiccacheapp/info.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ComicBookDetail extends StatelessWidget {
+  const ComicBookDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
