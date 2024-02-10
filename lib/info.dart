e2c660ca2fa8a105c055092d09cc068def10f520
Tony Nguyen <tonyqnguyen2003@gmail.com>
2024-02-09 19:41:24 -0500

    added the main classes to the info page

--- lib/info.dart
+++ lib/info.dart
@@ -24,11 +24,35 @@ class Book {
   String title;
   String description;
   String rating;
-  String image
+  String image;
   Author author; // implement author later 
 
   Book(this.title, this.description, this.author, this.rating, this.image);
-  
+
 }
+
+//implement getBook here
+
+class Author { 
+  
+  String name;
+  int numBooks;
+  String image; 
+
+  Author(this.name, this.numBooks, this.image);
+
+}
+
+// implement getAuthor here
+
+class Filter { 
+
+  String name;
+
+  Filter(this.name);
+
+}
+
+// implement getFilter here
 
 
